0e1fc003ad1c8a6fa1de9c8a95d216e3c2c79e43
yangyang02272003 <126850905+yangyang02272003@users.noreply.github.com>
2023-05-05 21:08:12 +0800

    实现第一题时上一题按钮不可见

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const questions = window['questions'] || {};
-let questionIndex;
-
-function ShowQuestionOfIndex(index) {
-	const question = questions[index];
-	const $questionText = document.getElementById('question');
-	$questionText.innerText = question.text;
-}
-
-function ShowPrev() {
-	--questionIndex;
-	ShowQuestionOfIndex(questionIndex);
-}
-
-function ShowNext() {
-	++questionIndex;
-	ShowQuestionOfIndex(questionIndex);
-}
-
-function SelectA() {
-	// TODO
-}
-
-function SelectB() {
-	// TODO
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+	const questions = window['questions'] || {};
+	let questionIndex;
+
+	// DOM elements
+	const $questionText = document.getElementById('question');
+	const $buttonPrev = document.getElementById('button-prev');
+
+	function ShowQuestionOfIndex(index) {
+		const question = questions[index];
+		$questionText.innerText = question.text;
+
+		if(index == 0) {
+			$buttonPrev.disabled = true;
+		}
+		else {
+			$buttonPrev.disabled = false;
+		}
+	}
+
+	window.ShowPrev = function ShowPrev() {
+		--questionIndex;
+		ShowQuestionOfIndex(questionIndex);
+	};
+
+	window.ShowNext = function ShowNext() {
+		++questionIndex;
+		ShowQuestionOfIndex(questionIndex);
+	};
+
+	window.SelectA = function SelectA() {
+		// TODO
+	};
+
+	window.SelectB = function SelectB() {
+		// TODO
+	};
+
 	questionIndex = 0;
 	ShowQuestionOfIndex(questionIndex);
 });
